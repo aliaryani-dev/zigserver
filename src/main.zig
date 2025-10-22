b81a53a4b428079df79e2f8c9d8768e0d5f87f24
Ali Aryani <lenesis@proton.me>
2025-10-22 16:39:11 +0330

    first main() implementation

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,12 @@ const SocketConf = @import("config.zig");
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
+
+pub fn main() !void {
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server Address: {any}\n", .{socket._address});
+    try stdout.flush();
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    _ = connection;
+}
