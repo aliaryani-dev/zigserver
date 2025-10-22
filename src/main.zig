fe4761d96e1c664d7c19307426b5d8bb6ef870ae
Ali Aryani <lenesis@proton.me>
2025-10-22 18:26:58 +0330

    add read to main()

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
 var stdout_buffer:[1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
@@ -10,5 +11,12 @@ pub fn main() !void {
     try stdout.flush();
     var server = try socket._address.listen(.{});
     const connection = try server.accept();
-    _ = connection;
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+
+    try stdout.print("{s}\n",.{buffer});
+    try stdout.flush();
 }
